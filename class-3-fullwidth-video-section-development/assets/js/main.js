1edce0732e677fc62f92558889c5e5d7bf0ebe17
smanwarulislam <engg.anwarbd@gmail.com>
2019-02-03 18:12:02 +0600

    class 3 fullwidth video section development added

---
+++ class-3-fullwidth-video-section-development/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".video-play-btn").magnificPopup({
+            type: 'video'
+        });
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
