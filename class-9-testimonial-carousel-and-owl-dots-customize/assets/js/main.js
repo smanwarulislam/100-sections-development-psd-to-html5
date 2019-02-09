9699e7334ec294e634911c4e9f617c40b3cc909e
smanwarulislam <engg.anwarbd@gmail.com>
2019-02-09 15:35:12 +0600

    class 9 testimonial carousel and owl dots customize added

---
+++ class-9-testimonial-carousel-and-owl-dots-customize/assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+
+        $(".client-testimonial-carousel").owlCarousel({
+            items: 3,
+            margin: 30,
+            nav: false,
+            dots: true,
+            loop: true
+        });
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
